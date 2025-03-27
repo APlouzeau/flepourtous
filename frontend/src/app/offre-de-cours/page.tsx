becef1127ac2f560ed452933e743f37f0b6b4171
Plouzeau Alexandre <plouzeau.alexandre@outlook.com>
2025-03-27 21:20:43 +0100

    Update page.tsx

--- frontend/src/app/offre-de-cours/page.tsx
+++ frontend/src/app/offre-de-cours/page.tsx
@@ -3,13 +3,13 @@ import type { lessons } from "@/app/types/lessons";
 
 export default async function OfferPage() {
     console.log("fonction");
-    const response = await fetch("http://flepourtous.localhost/api/lessons");
+    const response = await fetch("http://flepourtous.plouzor.fr/api/lessons");
     console.log(response);
     const lessons: lessons = await response.json();
     console.log("lessons", lessons);
     return (
         <>
-            <h1 className="text-5xl">Ceci est un test</h1>
+            <h1 className="text-5xl">Ceci est un test de déploiement automatique</h1>
             <h2 className="mt-12 text-5xl font-bold text-center">Offre de cours</h2>
             <div className="flex flex-wrap lg:flex-nowrap justify-center gap-8">
                 {lessons.map((lesson, index) => (
