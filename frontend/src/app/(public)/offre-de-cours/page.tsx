9681a2edb83880d14d6211a51579d37909320cbb
Alexandre Alexandre <plouzeau.alexandre@outlook.com>
2025-04-27 18:25:55 +0200

    :hammer: improve code and props to fetch  lessons display

--- frontend/src/app/(public)/offre-de-cours/page.tsx
+++ frontend/src/app/(public)/offre-de-cours/page.tsx
@@ -1,16 +1,17 @@
 import CardLesson from "@/app/components/front/CardLesson";
-import type { lessons } from "@/app/types/lessons";
+import type { LessonProps } from "@/app/types/lessons";
+import axios from "axios";
 
 export default async function OfferPage() {
-    const response = await fetch(`${process.env.API_URL}/lessons`);
-    const lessons: lessons = await response.json();
+    const response = await axios.get(`${process.env.API_URL}/lessons`);
+    const lessons: LessonProps[] = await response.data;
     return (
         <>
             <h2 className="mt-12 text-5xl font-bold text-center">Offre de cours</h2>
             <div className="flex flex-wrap lg:flex-nowrap justify-center gap-8">
-                {lessons.map((lesson, index) => (
+                {lessons.map((lesson) => (
                     <CardLesson
-                        key={index}
+                        key={lesson.id_lesson}
                         title={lesson.title}
                         shortDescription={lesson.shortDescription}
                         imagePath={lesson.imagePath}
