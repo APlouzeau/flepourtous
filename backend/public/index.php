73c52802ae4573254f5b239436f404f8203723d6
Alexandre Alexandre <plouzeau.alexandre@outlook.com>
2025-05-02 22:46:08 +0200

    :bug: fix environment varibale request

--- backend/public/index.php
+++ backend/public/index.php
@@ -3,7 +3,7 @@
 
 session_start();
 /* } */
-header("Access-Control-Allow-Origin: http://localhost:3000");
+//header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
