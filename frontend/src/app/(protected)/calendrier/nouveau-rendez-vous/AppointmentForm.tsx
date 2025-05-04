13b9c55698b16ff32b759b86dbf799fa0c4c66e5
Alexandre Alexandre <plouzeau.alexandre@outlook.com>
2025-05-04 22:09:54 +0200

    :bug: fix import for typescript

--- frontend/src/app/(protected)/calendrier/nouveau-rendez-vous/AppointmentForm.tsx
+++ frontend/src/app/(protected)/calendrier/nouveau-rendez-vous/AppointmentForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { getAvailableTimeSlots, registerAppointment } from "./AppointmentAction";
-import { useEffect, useId } from "react";
+import { useEffect } from "react";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { useState } from "react";
