34926e4b3f53907783d464ac0eb9bcb15c774ee3
Alexandre Alexandre <plouzeau.alexandre@outlook.com>
2026-04-16 11:35:14 +0200

    ressources pag traduction ok

--- frontend/src/locales/en/index.ts
+++ frontend/src/locales/en/index.ts
@@ -1,11 +1,13 @@
-import common from './common';
-import footer from './footer';
-import header from './header';
-import homePage from './homePage';
+import common from "./common";
+import footer from "./footer";
+import header from "./header";
+import homePage from "./homePage";
+import ressources from "./ressources";
 
 export default {
     ...common,
     ...footer,
     ...header,
     ...homePage,
+    ...ressources,
 } as const;
---
+++ frontend/src/locales/fr/ressources.ts
@@ -0,0 +1,41 @@
+export default {
+    ressources: {
+        title: "Ressources utilisées",
+        introduction:
+            "Pour proposer des cours dynamiques, variés et adaptés aux besoins de chaque apprenant, j’utilise un large éventail de ressources pédagogiques.",
+        subTtitle1: "Documents personnalisés",
+        content1:
+            "Je conçois moi-même de nombreux supports (fiches de vocabulaire, activités de grammaire, jeux, mises en situation, etc.) afin de m’adapter au niveau, aux centres d’intérêt et aux objectifs de chaque élève. Ces documents sont pensés pour favoriser l’interaction, l’autonomie et le plaisir d’apprendre.",
+        subTtitle2: "Ressources en ligne",
+        content2:
+            "Des extraits d’articles, des vidéos, des podcasts, des images ou des documents issus de la vie quotidienne (menus, publicités, horaires, formulaires...) viennent enrichir les cours pour une immersion dans le français réel et vivant.",
+        subTtitle3: "Manuels et ouvrages de référence",
+        content3:
+            "Je m’appuie sur des manuels de référence reconnus pour la qualité de leur contenu et leur efficacité pédagogique. Ces ressources sont sélectionnées avec soin pour garantir des cours de qualité, riches et stimulants, quel que soit le niveau.",
+        bookSection: {
+            title: "Voici quelques-uns des ouvrages que j’utilise régulièrement en fonction du niveau et des objectifs des apprenants",
+            books: [
+                {
+                    title: "Édito",
+                    description:
+                        "(Édition Didier) : une méthode actuelle, idéale pour développer les compétences générales tout en explorant la culture francophone.",
+                },
+                {
+                    title: 'La série "Progressive"',
+                    description:
+                        " (CLE International) : des ouvrages progressifs par niveau pour travailler la grammaire, le vocabulaire, la conjugaison, la compréhension écrite/orale, etc.",
+                },
+                {
+                    title: "Les 100% DELF",
+                    description:
+                        "(Édition Didier) : pour s’entraîner de manière intensive et ciblée à chaque épreuve du DELF, avec des stratégies et des mises en situation proches des examens officiels.",
+                },
+                {
+                    title: "Les ABC DELF",
+                    description:
+                        "(Éditions Nathan) : pour se préparer efficacement aux examens du DELF avec des explications claires, des conseils méthodologiques et des exercices corrigés.",
+                },
+            ],
+        },
+    },
+} as const;
